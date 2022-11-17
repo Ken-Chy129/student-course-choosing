769b3c4ab8c0401eb230aa25ecbf520f55a97aa6
Ken-Chy129 <1294967895@qq.com>
2022-11-17 17:19:01 +0800

    数据脱敏

--- src/main/java/cn/ken/student/rubcourse/common/entity/Result.java
+++ src/main/java/cn/ken/student/rubcourse/common/entity/Result.java
@@ -3,6 +3,7 @@ package cn.ken.student.rubcourse.common.entity;
 import cn.ken.student.rubcourse.common.enums.ErrorCodeEnums;
 import com.alibaba.fastjson.JSON;
 import lombok.Data;
+import red.zyc.desensitization.Sensitive;
 
 import java.io.Serializable;
 
@@ -36,7 +37,7 @@ public class Result implements Serializable {
     }
 
     public static Result success(Object data) {
-        return new Result(ErrorCodeEnums.SUCCESS.getCode(), ErrorCodeEnums.SUCCESS.getDesc(), JSON.toJSON(data));
+        return new Result(ErrorCodeEnums.SUCCESS.getCode(), ErrorCodeEnums.SUCCESS.getDesc(), JSON.toJSON(Sensitive.desensitize(data)));
     }
 
     public static Result fail() {
@@ -45,5 +46,13 @@ public class Result implements Serializable {
 
     public static Result fail(ErrorCodeEnums enums) {
         return new Result(enums.getCode(), enums.getDesc(), null);
+    }
+
+    public static Result fail(String code, String desc) {
+        return new Result(code, desc, null);
+    }
+    
+    public static Result fail(Object exception) {
+        return new Result(ErrorCodeEnums.ERROR.getCode(), ErrorCodeEnums.ERROR.getDesc(), exception);
     }
 }
