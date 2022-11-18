c9de72e98ef757c8d20b0d29571e0335468ce285
Ken-Chy129 <1294967895@qq.com>
2022-11-18 21:23:20 +0800

    通识课类型接口

--- src/main/java/cn/ken/student/rubcourse/controller/CourseGeneralTypeController.java
+++ src/main/java/cn/ken/student/rubcourse/controller/CourseGeneralTypeController.java
@@ -1,8 +1,16 @@
 package cn.ken.student.rubcourse.controller;
 
+import cn.ken.student.rubcourse.common.entity.Result;
+import cn.ken.student.rubcourse.service.ICourseGeneralTypeService;
+import io.swagger.annotations.ApiModelProperty;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * <p>
@@ -13,7 +21,22 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2022-11-16
  */
 @RestController
-@RequestMapping("/rubcourse/courseGeneralType")
+@RequestMapping("/courseGeneralType")
 public class CourseGeneralTypeController {
+    
+    @Autowired
+    private ICourseGeneralTypeService courseGeneralTypeService;
+    
+    @GetMapping("list")
+    @ApiModelProperty("查看通识选修课类别表")
+    public Result getCourseGeneralTypeList(HttpServletRequest httpServletRequest) {
+        return courseGeneralTypeService.getCourseGeneralTypeList(httpServletRequest);
+    }
+
+    @PostMapping("addCourseGeneralType")
+    @ApiModelProperty("增加通识选修课类别")
+    public Result addCourseGeneralType(HttpServletRequest httpServletRequest, String generalTypeName) {
+        return courseGeneralTypeService.addCourseGeneralType(httpServletRequest, generalTypeName);
+    }
 
 }
--- src/main/java/cn/ken/student/rubcourse/service/impl/CourseGeneralTypeServiceImpl.java
+++ src/main/java/cn/ken/student/rubcourse/service/impl/CourseGeneralTypeServiceImpl.java
@@ -1,10 +1,16 @@
 package cn.ken.student.rubcourse.service.impl;
 
+import cn.ken.student.rubcourse.common.entity.Result;
 import cn.ken.student.rubcourse.entity.CourseGeneralType;
 import cn.ken.student.rubcourse.mapper.CourseGeneralTypeMapper;
 import cn.ken.student.rubcourse.service.ICourseGeneralTypeService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +23,20 @@ import org.springframework.stereotype.Service;
 @Service
 public class CourseGeneralTypeServiceImpl extends ServiceImpl<CourseGeneralTypeMapper, CourseGeneralType> implements ICourseGeneralTypeService {
 
+    @Autowired
+    private CourseGeneralTypeMapper courseGeneralTypeMapper;
+    
+    @Override
+    public Result getCourseGeneralTypeList(HttpServletRequest httpServletRequest) {
+        List<CourseGeneralType> courseGeneralTypeList = courseGeneralTypeMapper.selectList(new QueryWrapper<>());
+        return Result.success(courseGeneralTypeList);
+    }
+
+    @Override
+    public Result addCourseGeneralType(HttpServletRequest httpServletRequest, String generalTypeName) {
+        CourseGeneralType courseGeneralType = new CourseGeneralType();
+        courseGeneralType.setTypeName(generalTypeName);
+        courseGeneralTypeMapper.insert(courseGeneralType);
+        return Result.success();
+    }
 }
