568b392b899c78295ca2f80dc7937e2ec56e5406
Ken-Chy129 <1294967895@qq.com>
2023-02-24 18:31:25 +0800

    引入分页插件

--- src/main/java/cn/ken/student/rubcourse/config/MyBatisPlusConfig.java
+++ src/main/java/cn/ken/student/rubcourse/config/MyBatisPlusConfig.java
@@ -1,7 +1,10 @@
 package cn.ken.student.rubcourse.config;
 
 import cn.ken.student.rubcourse.handle.FillMetaObjectHandle;
+import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,5 +22,13 @@ public class MyBatisPlusConfig {
     @Bean
     public MetaObjectHandler metaObjectHandler() {
         return new FillMetaObjectHandle();
+    }
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        // 分页插件
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
     }
 }
