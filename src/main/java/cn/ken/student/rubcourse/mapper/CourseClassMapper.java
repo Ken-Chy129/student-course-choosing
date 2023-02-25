c71025213fb12a06ffe6563a44bd76fb2dde31db
Ken-Chy129 <1294967895@qq.com>
2023-02-25 22:50:50 +0800

    更换联表查询为单表查询

--- src/main/java/cn/ken/student/rubcourse/mapper/CourseClassMapper.java
+++ src/main/java/cn/ken/student/rubcourse/mapper/CourseClassMapper.java
@@ -1,28 +1,21 @@
 package cn.ken.student.rubcourse.mapper;
 
-import java.util.List;
-
-import cn.ken.student.rubcourse.model.dto.req.ClassCourseListReq;
-import cn.ken.student.rubcourse.model.dto.resp.ClassCourseListResp;
-import cn.ken.student.rubcourse.model.dto.resp.CourseClassInfoResp;
-import cn.ken.student.rubcourse.model.dto.req.CourseClassListReq;
+import cn.ken.student.rubcourse.model.entity.CourseClass;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
-import cn.ken.student.rubcourse.model.entity.CourseClass;
+
+import java.util.List;
 
 /**
  * <pre>
  * <p>课程信息表(SccCourseInfo)表数据库访问层</p>
  * </pre>
+ *
  * @author <a href="https://github.com/kil1ua">Ken-Chy129</a>
- * @date  ${DATE} ${TIME}
+ * @date ${DATE} ${TIME}
  */
 @Mapper
 public interface CourseClassMapper extends BaseMapper<CourseClass> {
-
-    List<CourseClassInfoResp> getCourseInfoPage(CourseClassListReq courseClassListReq);
-    
-    List<ClassCourseListResp> getCourseClassInfoList(ClassCourseListReq classCourseListReq);
 
     List<CourseClass> selectByCourseId(String id);
 }
